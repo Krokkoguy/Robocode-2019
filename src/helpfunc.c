3083efd35edfb1d3c2bb7515ebe0d9503c2cb701
Ask Hetland Berentsen <askberentsen@gmail.com>
2019-04-22 17:24:15 +0200

    Added a function to make the sensor readings more usable.
    Reduces the sensor array output to a usable struct

--- src/helpfunc.c
+++ src/helpfunc.c
@@ -1,21 +1,69 @@
-// følg linje og stopp på enkoder
-// følg linje og stopp på tid
-// følg linje og stopp på port
-// følg linje og stopp på kryss
+// fï¿½lg linje og stopp pï¿½ enkoder
+// fï¿½lg linje og stopp pï¿½ tid
+// fï¿½lg linje og stopp pï¿½ port
+// fï¿½lg linje og stopp pï¿½ kryss
+
+#define SENSOR_SIZE 8
+#define SENSOR_POSITIVE 0
+
+struct SensorRead{
+  unsigned short width;
+  float direction;
+}
+
+/* Reads the output of a sensor array and calculates a value to use when turning.
+ * Reduces the output of the sensors to a usable number.
+ * Ignores false positives.
+ */
+struct SensorRead sensorRead( int sensorArr[SENSOR_SIZE] ){
+
+  struct sensorRead reading;
+
+  int index = 0;
+  int width = 0;
+
+  int maxIndex = index;
+  int maxWidth = width;
+
+  /* Iterate over sensorArr to find the largest consecutive chunks of positive readings */
+  for( int i = 0; i < SENSOR_SIZE; i++ ){
+
+    if( sensorArr[i] == SENSOR_POSITIVE ){
+
+      if( width == 0 ){
+        index = i;
+      }
+
+      ++width;
+    }
+    else if ( width > maxWidth ){
+      maxIndex = index;
+      maxWidth = width;
+    }
+    else{
+      count = 0;
+    }
+  }
+
+  reading.width = maxWidth;
+  reading.direction = maxWidth == 0 ? (maxIndex + (maxWidth/2.0)) - (SENSOR_SIZE/2.0) : 0.0;
+
+  return reading;
+}
 
 void DriveOnEncoder(int tics)
 {
 	Stop();
 	ResetEncoder();
-	
+
 	SERVOSet(servoLeft, spd1);
 	SERVOSet(servoRight, spd1);
-	
+
 	while (GetEncoder(AVERAGE) < tics)
 	{
 		OSSleep(1);
 	}
-	
+
 	Stop();
 	return;
 }
@@ -24,13 +72,13 @@ void FollowLineOnEncoder(int tics, int lr)
 {
 	Stop();
 	ResetEncoder();
-	
+
 	while (GetEncoder(AVERAGE) < tics)
 	{
 		FollowLine(lr);
 		OSSleep(1);
 	}
-	
+
 	Stop();
 	return;
 }
@@ -39,16 +87,16 @@ void DriveOnTime(int time)
 {
 	int counter = 0;
 	Stop();
-	
+
 	SERVOSet(servoLeft, spd1)
 	SERVOSet(servoRight, spd1);
-	
+
 	while (counter < time)
 	{
 		counter++;
 		OSSleep(1);
 	}
-	
+
 	Stop();
 	return;
 }
@@ -58,14 +106,14 @@ void FollowLineOnTime(int time, int lr)
 	int counter = 0;
 	Stop();
 	ResetEncoder(1);
-	
+
 	while (counter < time)
 	{
 		FollowLine(lr);
 		counter++;
 		OSSleep(1);
 	}
-	
+
 	Stop();
 	return;
 }
