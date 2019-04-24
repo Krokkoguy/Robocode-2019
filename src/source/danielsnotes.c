32877d733e3908dc6600a3a9d9a00efa8d0f29d7
Daniel <daniel.ch.nilsen@gmail.com>
2019-04-24 21:55:10 +0200

    Added personal notes
    4 backup...

---
+++ src/source/danielsnotes.c
@@ -0,0 +1,32 @@
+/*
+  This is just my personal notes (Hi, im Daniel, nice to meet you?!)
+  in c-format for automatic syntax-highligting.
+
+  What did the fish say when it swam into a wall?
+  - dam!
+*/
+
+
+/*
+Kjøre frem:
+  Følg linje til frontsensor registrerer at man er nære
+  roter på plass mot Venstre
+
+    Roter og følg til enden av dør:
+    Les og sammenlign høyre sensor, når laveste verdi er funnet og passert roter tilbake til laveste verdi
+    Kjør til høyre avstand er uendelig stor
+
+    Sving og finn "senter på boksen":
+    Roter 90 grader høyre
+    Kjør litt frem (resultat på høyre sensor i ca 1 sek?)
+
+    Kjør inntil:
+    Roter høyre til minimum avstand på frontsensor
+    kjør nære
+
+    Roter og åpne dør:
+    Roter høyre til venstre sensor er minimum
+    Kjør frem til verdi frontsensor er uendelig
+
+
+*/
